80d8bee42aa7c8e5afab8377f36dd160617492d3
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-05-11 11:06:11 -0400

    Add booth.css, modify index.html index.css app.js (add test buttons fetch, ajax and axios) booth.html (add idbooth form)

--- EC_Website/website/app.js
+++ EC_Website/website/app.js
@@ -1,23 +1,83 @@
-var totalAPI = "https://xul88ylpzf.execute-api.us-east-2.amazonaws.com/prod/total"
-//var tableGlobal = document.getElementById('#tableGlobal')
+var totalAPI = "https://j4220em7i6.execute-api.us-east-2.amazonaws.com/prod/total"
+var funAPI = "https://mindicador.cl/api/dolar"
+//var totalAPI = "total.json"
+var tableGlobal = document.getElementById('tableGlobal')
+var button = document.getElementById('button')
 
-/* fetch(totalAPI, {mode: 'no-cors'})
-  .then( res => res.ok ? Promise.resolve(res) : Promise.reject(res) )
+console.log("js funcionando");
 
-  .then( res => res.text() )
-  .then( json => {
-    console.log(json);
-    //tabla(json)
-  })*/
+function fetchfun() {
+  console.log("fetch");
 
-//function tabla(json) {
-//}
+  fetch(funAPI)
+  .then( res => res.json() )
+  .then( datos => {
+    console.log(datos);
+  })
 
-axios({
-  method: 'GET',
-  url: totalAPI
-}).then(res => {
-  console.log(res)
-}).catch(e => {
-  console.log(e);
-})
+}
+
+function ajaxfun(){
+  console.log("ajax")
+
+  const api = new XMLHttpRequest();
+  api.open('GET', funAPI, true);
+  api.send();
+  api.onreadystatechange = function() {
+    if(this.status == 200 && this.readyState == 4) {
+      let datos = JSON.parse(this.responseText);
+      console.log(datos);
+    }
+  }
+
+}
+
+function axiosfun() {
+  console.log("axios");
+
+  axios({
+    method: 'GET',
+    url: funAPI
+  }).then(res => {
+    console.log(res.data)
+  })
+
+}
+
+function fetchee() {
+  console.log("fetch");
+
+  fetch(totalAPI)
+  .then( res => res.json() )
+  .then( datos => {
+    console.log(datos);
+  })
+
+}
+
+function ajaxee(){
+  console.log("ajax")
+
+  const api = new XMLHttpRequest();
+  api.open('GET', totalAPI, true);
+  api.send();
+  api.onreadystatechange = function() {
+    if(this.status == 200 && this.readyState == 4) {
+      let datos = JSON.parse(this.responseText);
+      console.log(datos);
+    }
+  }
+
+}
+
+function axiosee() {
+  console.log("axios");
+
+  axios({
+    method: 'GET',
+    url: totalAPI
+  }).then(res => {
+    console.log(res.data)
+  })
+
+}
