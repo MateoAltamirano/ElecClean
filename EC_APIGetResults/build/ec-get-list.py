6d36139dcf34130584cd8e05f59bc9a5cb2fcaef
EC2 Default User <ec2-user@ip-172-31-26-134.us-east-2.compute.internal>
2020-05-11 22:21:29 +0000

    hola

--- EC_APIGetResults/build/ec-get-list.py
+++ EC_APIGetResults/build/ec-get-list.py
@@ -13,26 +13,94 @@ dynamodb = boto3.resource('dynamodb')
 elecclean= dynamodb.Table('ElecClean')
 
 def handler(event, context):
-    try:
-        items2 = elecclean.scan(
-            IndexName='fraudeGSI'
-        )
-        val =json.dumps(items2['Items'], indent=2, default=decimal_default)
-        val2 = json.loads(val)
-        return {
-            'statusCode': 200,
-            'body': json.dumps(val2),
-            "headers": {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+    data=event['queryStringParameters']['list']
+    if data=="ciudad":
+        try:
+            items2 = elecclean.scan()
+            arrayResp=[]
+            for item in items2['Items']:
+                if item['PK'][0:4]=="ciu-":
+                    arrayResp.append(item)
+            val =json.dumps(arrayResp, indent=2, default=decimal_default)
+            val2 = json.loads(val)
+            return {
+                'statusCode': 200,
+                'body': json.dumps(val2),
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
             }
-        }
-    except ClientError:
+        except ClientError:
+            return {
+                'statusCode': 500,
+                'body': 'Error querying database',
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+    elif data=="colegio":
+        try:
+            items2 = elecclean.scan()
+            arrayResp=[]
+            for item in items2['Items']:
+                if item['PK'][0:4]=="col-":
+                    arrayResp.append(item)
+            val =json.dumps(arrayResp, indent=2, default=decimal_default)
+            val2 = json.loads(val)
+            return {
+                'statusCode': 200,
+                'body': json.dumps(val2),
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+        except ClientError:
+            return {
+                'statusCode': 500,
+                'body': 'Error querying database',
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+    elif data=="candidato":
+        try:
+            items2 = elecclean.scan()
+            arrayResp=[]
+            for item in items2['Items']:
+                if item['PK'][0:9]=="candidato":
+                    arrayResp.append(item)
+            val =json.dumps(arrayResp, indent=2, default=decimal_default)
+            val2 = json.loads(val)
+            return {
+                'statusCode': 200,
+                'body': json.dumps(val2),
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+        except ClientError:
+            return {
+                'statusCode': 500,
+                'body': 'Error querying database',
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+    else:
         return {
             'statusCode': 500,
-            'body': 'Error querying database',
+            'body': 'Invalid command',
             "headers": {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
             }
         }
+        
+    
+    
