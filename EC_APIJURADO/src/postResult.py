c6a6c5b3595759c8a1983ce260b0dbfc9630f1f5
EC2 Default User <ec2-user@ip-172-31-35-101.us-east-2.compute.internal>
2020-05-12 14:20:49 +0000

    Publicacion de resultados de una mesa

---
+++ EC_APIJURADO/src/postResult.py
@@ -0,0 +1,47 @@
+import json
+import base64
+import boto3
+import io
+import json 
+from PIL import Image
+from requests_toolbelt.multipart import decoder
+
+
+
+s3 = boto3.resource('s3')
+dynamodb = boto3.resource('dynamodb')
+eleccleanDB = dynamodb.Table('ElecClean')
+
+def handler(event, context):
+    body = event["body"]
+
+    content_type = event["headers"]["Content-Type"]
+
+    body_dec = base64.b64decode(body)
+
+    multipart_data = decoder.MultipartDecoder(body_dec, content_type)
+    binary_content = []
+
+    for part in multipart_data.parts:
+        binary_content.append(part.content)
+    
+    imageStream = io.BytesIO(binary_content[0])
+    imageFile = Image.open(imageStream)
+    imageFile.save(imageStream,"JPEG")
+    imageStream.seek(0)
+    
+    request = json.loads(binary_content[1])
+    if not ("PK" in request and "colegio-ID" in request and "ciudad-ID" in request):
+        return{
+            'statusCode': 400,
+            'body': "Bad Request"
+        }
+    
+    s3.Bucket('images-elecclean').put_object(ACL='public-read',Key=request["PK"]+'.jpeg', Body=imageStream)
+    
+    eleccleanDB.put_item(Item=request)
+    
+    return {
+     'statusCode': 200,
+     'body': json.dumps(request)
+    }
